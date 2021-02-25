31bd83597ab2bddc5708770d65ba9e87219ee794
Rafael Moreno <Rafael Moreno@LAPTOP-ROGBR6G0>
2021-02-24 20:52:17 -0600

    tarea

---
+++ src/numpares60066/NumPares60066.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numpares60066;
+
+/**
+ *
+ * @author Rafael Moreno
+ */
+public class NumPares60066 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num = 0;
+  
+        while (num < 48){
+            if (num % 2 == 0){
+            System.out.println(num);
+        }
+               num++;
+    }
+}
+}
